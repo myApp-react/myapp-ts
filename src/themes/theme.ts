2a99b285eaa4113db197f8cef83b047f75e1602f
daoliwoniu <1446854445@qq.com>
2019-10-30 13:48:30 +0800

    first commit

---
+++ src/themes/theme.ts
@@ -0,0 +1,14 @@
+
+export interface themeSettings {
+  Theme: any
+}
+
+export default {
+  Theme: {
+    'fill-mask': 'rgba(0, 0, 0, 0.7)',
+    'brand-primary': '#E9434F',
+    'brand-primary-tap': '#FFAA0F',
+    'modal-button-font-size': '16px',
+    'modal-button-height': '48px',
+  }
+} as themeSettings;
